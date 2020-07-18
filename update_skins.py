557b7966e05b2d2c8827b8079447054fda321321
Lejo1 <Lejo_1@web.de>
2020-07-18 19:54:29 +0200

    Update scripts

--- update_skins.py
+++ update_skins.py
@@ -1,6 +1,6 @@
-import sys, requests, hashlib
+import sys, requests, hashlib, base64
 
-print "Downloading skins from minetest.fensta.bplaced.net ..."
+print("Downloading skins from minetest.fensta.bplaced.net ...")
 r = requests.get('http://minetest.fensta.bplaced.net/api/v2/get.json.php?getlist&page=1&per_page=9999999999')
 
 if r.status_code != 200:
@@ -9,25 +9,25 @@ if r.status_code != 200:
 data = r.json()
 count = 0
 
-print "Writing to file and downloading previews ..."
+print("Writing to file and downloading previews ...")
 for json in data["skins"]:
-    raw_data = json["img"].decode('base64')
+    raw_data = base64.b64decode(json["img"])
     sha1 = hashlib.sha1(raw_data)
-    file = open("media/" + sha1.hexdigest(), "w")
-    file.write(raw_data)
+    file = open("media/" + sha1.hexdigest(), "wb")
+    file.write(bytearray(raw_data))
     file.close()
     id = str(json["id"])
     r2 = requests.get('http://minetest.fensta.bplaced.net/skins/1/' + id + ".png")
     if r.status_code == 200:
         data2 = r2.content
         sha12 = hashlib.sha1(data2)
-        file = open("media/" + sha12.hexdigest(), "w")
-        file.write(data2)
+        file = open("media/" + sha12.hexdigest(), "wb")
+        file.write(bytearray(data2))
         file.close()
 
     else:
-        print "Fetching of preview of skin " + id + "failed!"
+        print("Fetching of preview of skin " + id + "failed!")
 
     count += 1
 
-print "Fetched " + str(count) + " skins!"
+print("Fetched " + str(count) + " skins!")
