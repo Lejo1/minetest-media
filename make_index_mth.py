f21d4bf0bf95b5ce389e36f7ab23e86497f7c026
Lejo1 <Lejo_1@web.de>
2021-04-17 15:56:37 +0200

    Update indexing script to python 3

--- make_index_mth.py
+++ make_index_mth.py
@@ -1,15 +1,18 @@
 import os
+import codecs
+
+decode_hex = codecs.getdecoder("hex_codec")
 
 data = b"\x4d\x54\x48\x53\x00\x01"
 count = 0
 print("Reading and converting media files...")
-for filename in os.listdir("media"):
+for filename in os.listdir("m"):
     if filename != "index.mth":
-        data += filename.decode("hex")
+        data += decode_hex(filename)[0]
         count += 1
 
 print("Writing to file...")
-file = open("media/index.mth", "w")
+file = open("m/index.mth", "wb")
 file.write(data)
 file.close()
 print("Wrote " + str(count) + " medias to file.")
